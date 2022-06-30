42e47e7c2dc6828737e5acf05d3d0ab1d250da9a
pranavpatel <pranavpatel986@gmail.com>
2022-06-30 23:53:37 +0530

    second

--- src/main/java/com/simplilearn/JenkinsDemoApplication.java
+++ src/main/java/com/simplilearn/JenkinsDemoApplication.java
@@ -2,10 +2,18 @@ package com.simplilearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDemoApplication {
 
+	@GetMapping
+	public String message() {
+		return "welcome to Pranav patel PC";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
